3a7c8dbc193f67ef3f7517fa2594f18e6ba7db9f
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-04 00:00:59 +0530

    change the code sample to make the output look the following way

---
+++ Problems/Assignments/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        System.out.println(a + " " + b + " " + c);
+    }
+}
