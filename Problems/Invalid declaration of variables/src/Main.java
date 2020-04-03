119afade14c38aa02316734527f1b94864ad9dba
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-03 23:59:17 +0530

    given two variables a and b with invalid declarations, so add a type to them

---
+++ Problems/Invalid declaration of variables/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Add type to these two variables
+        long a, b;
+        a = 512343;
+        b = 3431231;
+
+        System.out.println(a + b);
+    }
+}
