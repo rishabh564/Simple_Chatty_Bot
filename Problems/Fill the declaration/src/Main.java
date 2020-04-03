8b2836b9ccbb1ca52319415a30fc679f5ea3b8ec
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-04 00:02:46 +0530

    given a variable, add the int type and assign the value 123456 to this variable

---
+++ Problems/Fill the declaration/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        long variable;  // Change this line
+        Scanner sc = new Scanner(System.in);
+        variable = sc.nextInt();
+        System.out.println(variable);
+    }
+}
+
