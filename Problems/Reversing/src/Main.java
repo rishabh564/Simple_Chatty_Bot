e4295f6923dc62397ba5f1056b811e97474635ae
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-04 14:30:54 +0530

    reverse variable is used to get a reverse no

---
+++ Problems/Reversing/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // put your code here
+        int a = scanner.nextInt();
+        int temp = 0;
+        int reverse = 0;
+
+    }
+}
