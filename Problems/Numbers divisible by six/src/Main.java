b7dacb5aa94f82ba0dd35c4db50da90569f019e2
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-04 18:37:10 +0530

    second for loop for getting values divisible by 6;

--- Problems/Numbers divisible by six/src/Main.java
+++ Problems/Numbers divisible by six/src/Main.java
@@ -11,4 +11,10 @@ class Main {
         }
         int[] arr = new int[a];
         int j = 0;
-
+        for (int i = 0; i < ar.length; i++) {
+            int div = ar[i] % 6;
+            if (div == 0) {
+                arr[j] = ar[i];
+                j += 1;
+            }
+        }
