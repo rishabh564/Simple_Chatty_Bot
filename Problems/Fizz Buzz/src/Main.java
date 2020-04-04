2b73221ca1d0241cec65902fb161a247708e44db
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-04 22:37:43 +0530

    if i%5 gives remainder 0 then print FizzBuzz

--- Problems/Fizz Buzz/src/Main.java
+++ Problems/Fizz Buzz/src/Main.java
@@ -5,3 +5,8 @@ class Main {
         Scanner sc = new Scanner(System.in);
         int n1 = sc.nextInt();
         int n2 = sc.nextInt();
+        for (int i = n1; i <= n2; i++) {
+            if ((i % 3) == 0) {
+                if ((i % 5) == 0) {
+                    System.out.println("FizzBuzz");
+                }
