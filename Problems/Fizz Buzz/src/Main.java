91153c90c4c8848593f7cf3394154bbfc80a5dd5
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-04 22:39:55 +0530

    if i%5 not gives remainder 0 then print value of i

--- Problems/Fizz Buzz/src/Main.java
+++ Problems/Fizz Buzz/src/Main.java
@@ -9,4 +9,15 @@ class Main {
             if ((i % 3) == 0) {
                 if ((i % 5) == 0) {
                     System.out.println("FizzBuzz");
+                } else {
+                    System.out.println("Fizz");
                 }
+            } else if ((i % 5) == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+            // put your code here
+        }
+    }
+}
