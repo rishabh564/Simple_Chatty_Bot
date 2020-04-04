9ef2c49c3c88b5126b4c38816d848ef4b17db8d0
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-04 14:07:43 +0530

    four int variables are used to get input

---
+++ Problems/Reading integer numbers/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+    }
