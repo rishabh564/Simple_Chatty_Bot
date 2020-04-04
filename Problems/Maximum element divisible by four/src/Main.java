827e59de83d234918f18e563851f00886505c917
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-04 17:00:36 +0530

    elememts of array are taken as input by the user

---
+++ Problems/Maximum element divisible by four/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int[] ar = new int[a];
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = sc.nextInt();
+        }
+
+    }
+}
