53c9b2a920af6e77ebe48c9498a61b063cbf3b18
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-04 14:05:21 +0530

    two int variables are used t0 get input

---
+++ Problems/How many nuts each squirrel will get/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // put your code here
+        Scanner sc = new Scanner(System.in);
+        int sq = sc.nextInt();
+        int nuts = sc.nextInt();
+
+    }
+}
