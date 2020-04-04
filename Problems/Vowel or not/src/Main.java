babdeb974dcf3b35300760a91a9d5a2964dc4bdd
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-05 01:47:26 +0530

    if conditions are false in method then return false value

--- Problems/Vowel or not/src/Main.java
+++ Problems/Vowel or not/src/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static boolean isVowel(char ch) {
         if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' || ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
             return true;
+        else
+            return false;
 
     }
 
