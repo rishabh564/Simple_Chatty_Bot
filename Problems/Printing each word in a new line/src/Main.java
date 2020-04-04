19e54aa368e0adbdcac4bb8d4584c2c5cc2df819
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-04 13:53:56 +0530

    five string variables are used to get input

---
+++ Problems/Printing each word in a new line/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        String s3 = sc.next();
+        String s4 = sc.next();
+        String s5 = sc.next();
+
+    }
