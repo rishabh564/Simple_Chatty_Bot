bd7bf7f47e8eff7095b66a55f6199c9eb4d0a8fd
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-04 12:41:56 +0530

    comment several lines so the program prints only three numbers

--- Problems/Three numbers/src/Main.java
+++ Problems/Three numbers/src/Main.java
@@ -6,7 +6,9 @@ public class Main {
         //System.out.println(2);
         //System.out.println(3);
         //System.out.println(4);
-
+        System.out.println(5);
+        System.out.println(6);
+        System.out.println(7);
         //System.out.println(8);
     }
 }
