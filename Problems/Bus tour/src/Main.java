27aee2155f9ffd46347e0489b0494516fa6258bf
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-05 00:49:07 +0530

    bridgeno variable is used to represent total no of bridges

--- Problems/Bus tour/src/Main.java
+++ Problems/Bus tour/src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int hbus = sc.nextInt();
-
+        int bridgeno = sc.nextInt();
     }
 }
