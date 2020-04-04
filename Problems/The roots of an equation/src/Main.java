5ca7b8e3383290ac8e6bbf2a1f77042a5bfea702
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-04 16:39:16 +0530

    calculate the roots of given cubic equation

--- Problems/The roots of an equation/src/Main.java
+++ Problems/The roots of an equation/src/Main.java
@@ -8,6 +8,15 @@ class Main {
         int b = sc.nextInt();
         int c = sc.nextInt();
         int d = sc.nextInt();
+        int m = 0;
+        for (int i = 0; i <= 1000; i++) {
+            if (a * i * i * i + b * i * i + c * i + d == 0) {
+                System.out.println(i);
+                m += 1;
+            }
+            if (m == 3)
+                break;
 
+        }
     }
 }
