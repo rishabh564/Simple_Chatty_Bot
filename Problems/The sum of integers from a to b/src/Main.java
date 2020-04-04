a0d3505e0355a9677f37f45f3f35344cad932e25
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-04 18:44:34 +0530

    for loop is used to get sum

--- Problems/The sum of integers from a to b/src/Main.java
+++ Problems/The sum of integers from a to b/src/Main.java
@@ -5,5 +5,9 @@ class Main {
         Scanner sc = new Scanner(System.in);
         long a = sc.nextInt();
         long b = sc.nextInt();
-
+        long sum = 0;
+        for (long i = a; i <= b; i++)
+            sum = sum + i;
+        System.out.println(sum);
     }
+}
