c1f13b991afbee609f1c6e0886cea361f991bd3b
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-04 14:34:37 +0530

    various variables are used to get time like hour, min, sec

---
+++ Problems/Difference of times/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // put your code here
+        int h1 = scanner.nextInt();
+        int m1 = scanner.nextInt();
+        int s1 = scanner.nextInt();
+        int h2 = scanner.nextInt();
+        int m2 = scanner.nextInt();
+        int s2 = scanner.nextInt();
+
+    }
+}
