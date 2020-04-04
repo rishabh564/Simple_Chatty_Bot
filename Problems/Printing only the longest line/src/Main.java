87edfbbe0dc873ecef33ad0e40503e282743a0da
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-04 13:21:14 +0530

    comment the lines required to get output

---
+++ Problems/Printing only the longest line/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //System.out.println("Print it!");
+        // System.out.println("No, print it!");
+        /* System.out.println("Uncomment this line!"); */
+
+    }
+}
