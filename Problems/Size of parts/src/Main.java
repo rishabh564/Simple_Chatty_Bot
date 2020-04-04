bddb6f3974a8c42d80da4b5f77dc504ec4b41d48
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-04 22:48:17 +0530

    for loop is used to update the variables

--- Problems/Size of parts/src/Main.java
+++ Problems/Size of parts/src/Main.java
@@ -8,3 +8,15 @@ class Main {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
+        int c = 0;
+        int c1 = 0;
+        int c2 = 0;
+        for (int j = 0; j < n; j++) {
+            if (arr[j] == 0) {
+                c++;
+            } else if (arr[j] == -1) {
+                c1++;
+            } else {
+                c2++;
+            }
+        }
