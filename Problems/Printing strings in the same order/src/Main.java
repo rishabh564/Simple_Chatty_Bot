f659aafc7cc67e44800112c73365d81c22f63fa8
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-04 13:50:42 +0530

    print statements are used to get output

--- Problems/Printing strings in the same order/src/Main.java
+++ Problems/Printing strings in the same order/src/Main.java
@@ -8,7 +8,10 @@ class Main {
         String s2 = sc.next();
         String s3 = sc.next();
         String s4 = sc.next();
-
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
 
     }
 }
