6f3dee27c90013f4b6546dd5dd77324e79635481
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-04 13:59:04 +0530

    some variables are used to get input

---
+++ Problems/Guest list of a hotel/src/Main.java
@@ -0,0 +1,19 @@
+//put imports you need here
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        String s3 = sc.next();
+        String s4 = sc.next();
+        String s5 = sc.next();
+        String s6 = sc.next();
+        String s7 = sc.next();
+        String s8 = sc.next();
+
+    }
+}
