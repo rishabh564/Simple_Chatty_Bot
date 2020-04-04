a99569f77a65bf2e133a76a51006525582618095
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-04 13:41:38 +0530

    various string variables are used to get input

---
+++ Problems/Print strings in the reverse order/src/Main.java
@@ -0,0 +1,13 @@
+//put imports you need here
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        String s3 = sc.next();
+
+    }
