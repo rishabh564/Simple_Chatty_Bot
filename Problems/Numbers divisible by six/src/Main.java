ec0d49a99c0e3c687610eadbe302815eddf48bea
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-04 18:35:52 +0530

    for loop is used to get values for array

--- Problems/Numbers divisible by six/src/Main.java
+++ Problems/Numbers divisible by six/src/Main.java
@@ -5,6 +5,7 @@ class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int[] ar = new int[a];
+
         for (int i = 0; i < ar.length; i++) {
             ar[i] = sc.nextInt();
         }
