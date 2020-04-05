a5abc9b7b62daa2c710f9e5aa1b92aa49197c5d2
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-05 14:01:20 +0530

    if n is 1 then break the loop

--- Problems/Collatz conjecture/src/Main.java
+++ Problems/Collatz conjecture/src/Main.java
@@ -6,7 +6,8 @@ class Main {
         int n = sc.nextInt();
         System.out.print(n + " ");
         while (true) {
-
+            if (n == 1)
+                break;
             else if (n % 2 == 0) {
                 n = n / 2;
                 System.out.print(n + " ");
