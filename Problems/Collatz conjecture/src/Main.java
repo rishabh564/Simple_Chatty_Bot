8fd3f52740f696a273b82749e0f3c7042879da57
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-05 14:00:30 +0530

    If n is an even number, divide it in half

--- Problems/Collatz conjecture/src/Main.java
+++ Problems/Collatz conjecture/src/Main.java
@@ -1,19 +1,17 @@
 import java.util.Scanner;
 
 class Main {
-    static void print(int n) {
-        while (n != 1) {
-            System.out.print(n + " ");
-            if ((n & 1) == 1)
-                n = 3 * n + 1;
-            else
-                n = n / 2;
-        }
-        System.out.print(n);
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        System.out.print(n + " ");
+        while (true) {
+
+            else if (n % 2 == 0) {
+                n = n / 2;
+                System.out.print(n + " ");
+            }
+
+        }
     }
 }
