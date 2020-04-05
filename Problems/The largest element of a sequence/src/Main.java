7ff4182de54f8f309da3f950f5731092f60f297d
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-05 16:37:50 +0530

    now elements are added to ArrayList arr

--- Problems/The largest element of a sequence/src/Main.java
+++ Problems/The largest element of a sequence/src/Main.java
@@ -1,13 +1,19 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 class Main {
     public static void main(String[] args) {
         // put your code here
         Scanner sc = new Scanner(System.in);
         int a = 0;
         ArrayList<Integer> arr = new ArrayList<>();
+
+        while (true) {
+            a = sc.nextInt();
+            if (a == 0)
+                break;
+            arr.add(a);
+        }
 
     }
 }
