b386bee698baa1181334ccf7eb3aa01586024c81
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-05 12:03:31 +0530

    if first position is the largest no

---
+++ Problems/Find the max of three numbers/src/Main.java
@@ -0,0 +1,9 @@
+public static int getNumberOfMaxParam(int a,int b,int c){
+        // write a body here
+        if(a>b&&b>c)
+        return 1;
+        if(a>c&&c>b)
+        return 1;
+
+
+        }
