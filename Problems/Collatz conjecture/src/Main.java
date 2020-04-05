1b6acd70794a27ec7f6241fe2d88d32a6f1469fe
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-05 14:00:51 +0530

    if it is odd, multiply it by 3 and add 1

--- Problems/Collatz conjecture/src/Main.java
+++ Problems/Collatz conjecture/src/Main.java
@@ -10,6 +10,9 @@ class Main {
             else if (n % 2 == 0) {
                 n = n / 2;
                 System.out.print(n + " ");
+            } else if (n % 2 != 0) {
+                n = (n * 3) + 1;
+                System.out.print(n + " ");
             }
 
         }
