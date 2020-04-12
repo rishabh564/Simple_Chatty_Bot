b112dcd6a009b1e87ece3f262307fae9b4f5d348
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-12 14:50:16 +0530

    some errors are removed

--- Problems/Comparing strings ignoring whitespaces/src/Main.java
+++ Problems/Comparing strings ignoring whitespaces/src/Main.java
@@ -4,8 +4,14 @@ class Main {
     public static void main(String[] args) {
         // put your code here
         Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        String b = sc.next();
-
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        String c = a.replaceAll("\\s+", "");
+        String d = b.replaceAll("\\s+", "");
+        if (c.equalsIgnoreCase(d))
+            System.out.println(true);
+        else
+            System.out.println(false);
     }
 }
+
